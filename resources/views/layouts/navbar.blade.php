3736035f890fccd3507c5ff0acb9aa2394e15c6a
masbarok193@gmail.com <masbarok193@gmail.com>
2023-08-28 11:50:56 +0700

    update href

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,5 @@
      <div class="navbar-brand">
-           <a style="text-decoration:none" href="/dash" >
+           <a style="text-decoration:none" href="/dashboard-app" >
             <span class="item-info">IC</span
             ><span class="text-warn">WAREHOUSE</span>
            </a> 
