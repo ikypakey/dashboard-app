2529464d48a0fef7f9942fea19f2393cd5124e46
ikypakey <mrizkimubarokk7@gmail.com>
2023-12-28 08:52:26 +0700

    rev title

--- resources/views/background.blade.php
+++ resources/views/background.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Bootstrap Homepage</title>
+    <title>Admin Homepage</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
